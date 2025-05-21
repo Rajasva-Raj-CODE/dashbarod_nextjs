c9fc17e3a5b8a47bc8ceb7cb644a53b78fd11b02
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-05-21 18:14:14 +0530

    charts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-import { Button } from "@/components/ui/button";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
 
 export default function Home() {
   return (
-  <div>
-  </div>
+    <div className="grid grid-cols-1 lg-grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+    </div>
   );
 }
